6cd7f2e511a1b236fb7696a73b603e46166bc91c
Craig Denson <dcd@coastallowclouds.com>
2022-08-24 15:59:06 -0700

    Eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    },
+
+    "globals": {
+       "BABYLON": "readonly",
+       "Turtle3d":  "readonly",
+       "Lsystem;":  "readonly",
+       "puts":  "readonly",
+       "LOGTAG_MORE":   "readonly",
+       "LOGTAG_MOST":   "readonly",
+       "MAIN_ALL":   "readonly",
+       "LSYS_PARSE":   "readonly",
+       "LSYS_IN_ITEMS":   "readonly",
+       "LSYS_IN_PROD":   "readonly",
+       "LSYS_IN_DECOMP":   "readonly",
+       "LSYS_IN_HOMO":   "readonly",
+       "LSYS_IN_WANT":   "readonly",
+       "LSYS_PARSE_PROD":   "readonly",
+       "LSYS_PARSE_MOD":   "readonly",
+       "LSYS_PARSE_SUCC":   "readonly",
+       "LSYS_REWRITE":   "readonly",
+       "LSYS_EXPAND":   "readonly",
+       "LSYS_MATCH":   "readonly",
+       "LSYS_CONTEXT":   "readonly",
+       "TRTL_CAPTURE":   "readonly",
+       "TRTL_MATERIAL":   "readonly",
+       "TRTL_TEXTURE":   "readonly",
+       "TRTL_TRACK":   "readonly",
+       "TRTL_CONTOUR":   "readonly",
+       "TRTL_DRAW":   "readonly",
+       "TRTL_POLYGON":   "readonly",
+       "TRTL_SETGET":   "readonly",
+       "NTRP_INIT":   "readonly",
+       "NTRP_SETTING":   "readonly",
+       "NTRP_MOTION":   "readonly",
+       "NTRP_HEADING":   "readonly",
+       "NTRP_SIZE":   "readonly",
+       "NTRP_BRANCH":   "readonly",
+       "NTRP_PROGRESS":   "readonly",
+       "NTRP_TRACKS":   "readonly",
+       "NTRP_CONTOUR":   "readonly",
+       "TRTL_HERMITE":   "readonly",
+       "LSYS_REWRITE_VERB":   "readonly"
+    }
+}
