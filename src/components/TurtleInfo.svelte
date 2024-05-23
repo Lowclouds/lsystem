e5658b3974b73c2d5a3d4b95b29949202ef2ce5b
Craig Denson <dcd@coastallowclouds.com>
2024-05-23 06:57:52 -0700

    Add camera position to turtle info
    Enables  user to manually position view and then copy into a view spec

--- src/components/TurtleInfo.svelte
+++ src/components/TurtleInfo.svelte
@@ -1,22 +1,27 @@
 <script>
   import {getContext, onMount} from 'svelte';
   import {createReactiveContext} from '/src/lib/reactiveContext.js';
+  import {camera} from '/src/lib/scene.js';
 
   // let turtle = getContext('turtle');
   let turtle ;
+  let cam ;
   let tid, tPos, tH, tL, tU;
+  let camPos=[], camTarget=[], posDir=[];
 
   let scene = getContext('scene');
   $: if ($scene) {
     turtle = Turtle3d.getFirstTurtle();
+    cam = camera;
   }
 
   let isShown; // = 'none';
   let doShowTurtleInfo = getContext('doShowTurtleInfo');
   
   $: if ( $doShowTurtleInfo ) { // || (tPos.x || tPos.y || tPos.z))
-    isShown = 'contents';
     updturtle();
+    //isShown = 'contents';
+    isShown = 'flex';
   } else {
     isShown = 'none';
   }
@@ -27,26 +32,50 @@
     tH = vround(turtle.getH(), 3);
     tL = vround(turtle.getL(), 3);
     tU = vround(turtle.getU(), 3);
+
+    camPos=[];
+    camTarget=[];
+    posDir=[];
+    vround(camera.position.clone(), 3).toArray(camPos);
+    vround(camera.target.clone(), 3).toArray(camTarget);
+    vround(camera.position.subtract(camera.target).normalize(), 3).toArray(posDir);
+
   }
 </script>
 
-<div id="turtleinfo" style="display: {isShown}">
-  <table class="tbl" id="turtleinfo">
-    <tr><td><button on:click={updturtle}>Update</button></td><td id="t1">{tid} </td></tr>
-    <tr><td >Pos</td><td id="t1P">{tPos}</td></tr>
-    <tr><td class="tH">H(eading)</td><td id="t1H" class="tH">{tH}</td></tr>
-    <tr><td class="tL">L(left)</td><td id="t1L" class="tL">{tL}</td></tr>
-    <tr><td class="tU">U(p)</td><td id="t1U" class="tU">{tU}</td></tr>
-  </table>
+<div style="display: {isShown}; width: 100%; justify-content: space-between;">
+  <div>
+    <table class="tbl" id="turtleinfo">
+      <tr><td><button on:click={updturtle}>Update</button></td><td id="t1">Turtle: {tid} </td></tr>
+      <tr><td >Pos</td><td id="t1P">{tPos}</td></tr>
+      <tr><td class="tH">H(eading)</td><td id="t1H" class="tH">{tH}</td></tr>
+      <tr><td class="tL">L(left)</td><td id="t1L" class="tL">{tL}</td></tr>
+      <tr><td class="tU">U(p)</td><td id="t1U" class="tU">{tU}</td></tr>
+    </table>
+  </div>
+  <div>
+    <table class="tbl" id="camerainfo">
+      <tr><td></td><td id="c1">Camera </td></tr>
+      <tr><td >Position</td><td id="c1P">{camPos}</td></tr>
+      <tr><td >Target</td><td id="c1T">{camTarget}</td></tr>
+      <tr><td >Direction</td><td id="c1D">{posDir}</td></tr>
+    </table>
+  </div>
+
 </div>
 
 <style>
   button {
     background-color: aquamarine;
     border-radius: 4px;
+    font-size: smaller;
+  }
+  table {
+    font-size: smaller;
   }
   td {
-    padding: 0 1em 0 .5em;
+    padding: 0 1em 0 .25em;
+    font-size: smaller;
   }
   .tH { color: red; }
   .tL { color: blue; }
